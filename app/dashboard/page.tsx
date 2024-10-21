072dcd203da539568e024f82182b7eb37617cbd9
Ahmed Shalaby <ahmedismaiel@live.com>
2024-10-21 21:29:29 +0300

    dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,53 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Chart1 } from "./Chart1";
+import { Chart2 } from "./Chart2";
+import { Chart3 } from "./Chart3";
+
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <div className="grid  auto-rows-min gap-4 md:grid-cols-3">
+            <Chart1 />
+            <Chart1 />
+            <Chart2 />
+          </div>
+          <Chart3 />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
